3087bf73468a245c1112ff37e1a487830641fe30
Sagar Bhatt <60082886+sagarbk0@users.noreply.github.com>
2021-01-17 23:39:22 -0500

    update

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from "react"
-import ImageBox from "./Components/Image"
-import Container from "react-bootstrap/Container"
-import Row from "react-bootstrap/Row"
-import Col from "react-bootstrap/Col"
+import React, { useEffect, useState } from "react";
+import ImageBox from "./Components/Image";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
 
 const App = () => {
   var request = new XMLHttpRequest();
@@ -22,8 +22,9 @@ const App = () => {
       for (var index in images.value) {
         var image = images.value[index];
 
-        setResults((oldResults) => [...oldResults, 
-          (<ImageBox
+        setResults((oldResults) => [
+          ...oldResults,
+          <ImageBox
             Name={image.name}
             Id={image.imageId}
             Image={image.contentUrl}
@@ -31,7 +32,7 @@ const App = () => {
             Function="search"
             Add={add}
             Remove={remove}
-          />)
+          />
         ]);
 
         if (collectionIds.includes(image.imageId)) {
@@ -46,13 +47,20 @@ const App = () => {
       if (event.key === "Enter" && event.target.nodeName === "INPUT") {
         var string =
           "https://bing-image-search1.p.rapidapi.com/images/search?q=" +
-          document.getElementById("searchName").value + "&imageType=" + 
+          document.getElementById("searchName").value +
+          "&imageType=" +
           document.querySelector('input[name="type"]:checked').value;
         request.open("GET", string);
-        request.setRequestHeader("x-rapidapi-key", "98f010221dmsh0cfbf7835e80775p143c34jsn618d0cf263c8");
-        request.setRequestHeader("x-rapidapi-host", "bing-image-search1.p.rapidapi.com");
-        
-        request.send(); 
+        request.setRequestHeader(
+          "x-rapidapi-key",
+          "98f010221dmsh0cfbf7835e80775p143c34jsn618d0cf263c8"
+        );
+        request.setRequestHeader(
+          "x-rapidapi-host",
+          "bing-image-search1.p.rapidapi.com"
+        );
+
+        request.send();
       }
     };
 
@@ -70,7 +78,7 @@ const App = () => {
       return m.imageId === event.target.id;
     });
 
-    var imageObject = 
+    var imageObject = (
       <ImageBox
         Name={image[0].name}
         Id={image[0].imageId}
@@ -79,7 +87,8 @@ const App = () => {
         Function="nomination"
         Add={add}
         Remove={remove}
-      />;
+      />
+    );
 
     collectionIds.push(event.target.id);
 
@@ -106,22 +115,67 @@ const App = () => {
 
   return (
     <Container>
-      <Row className="justify-content-md-center" w="2"><img src="https://i.ibb.co/N1Tt4Vy/logo.png" alt="logo"></img></Row>
-      <Row><p>Search for images and add them to your collection</p></Row>
+      <Row className="justify-content-md-center" w="2">
+        <img src="https://i.ibb.co/N1Tt4Vy/logo.png" alt="logo"></img>
+      </Row>
+      <Row>
+        <p>Search for images and add them to your collection</p>
+      </Row>
       <Row>
         <form>
           <input className="mr-2" d="searchName" placeholder="Search" />
-          <input className="mr-1" type="radio" id="All" name="type" value="All" checked />
-          <label className="mr-2" for="male">All</label>
-          <input className="mr-1" type="radio" id="Clipart" name="type" value="Clipart" />
-          <label className="mr-2" for="female">Clip art</label>
-          <input className="mr-1" type="radio" id="Line" name="type" value="Line" />
-          <label className="mr-2" for="other">Line art</label>
-          <input className="mr-1" type="radio" id="Photo" name="type" value="Photo" />
-          <label className="mr-2" for="other">Photo</label>
-          <input className="mr-1" type="radio" id="AnimatedGif" name="type" value="AnimatedGif" />
-          <label className="mr-4" for="other">GIF</label>
-          {/* Not functional currently, but would implement by storing username, password and user's collection in MongoDB */}
+          <input
+            className="mr-1"
+            type="radio"
+            id="All"
+            name="type"
+            value="All"
+            checked
+          />
+          <label className="mr-2" for="male">
+            All
+          </label>
+          <input
+            className="mr-1"
+            type="radio"
+            id="Clipart"
+            name="type"
+            value="Clipart"
+          />
+          <label className="mr-2" for="female">
+            Clip art
+          </label>
+          <input
+            className="mr-1"
+            type="radio"
+            id="Line"
+            name="type"
+            value="Line"
+          />
+          <label className="mr-2" for="other">
+            Line art
+          </label>
+          <input
+            className="mr-1"
+            type="radio"
+            id="Photo"
+            name="type"
+            value="Photo"
+          />
+          <label className="mr-2" for="other">
+            Photo
+          </label>
+          <input
+            className="mr-1"
+            type="radio"
+            id="AnimatedGif"
+            name="type"
+            value="AnimatedGif"
+          />
+          <label className="mr-4" for="other">
+            GIF
+          </label>
+          {/* Not functional currently, but would implement by storing username, password and user's collection array in MongoDB */}
           <input type="textField" placeHolder="username" />
           <input type="password" placeHolder="password" />
           <button>Save collection</button>
